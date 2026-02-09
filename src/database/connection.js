5fe71a6b099f9a8084e6f87a1af2613a9e5f1923
gr8-alizaidi <iamalizaidi110@gmail.com>
2026-02-09 17:41:33 +0530

    Test/comment update

--- src/database/connection.js
+++ src/database/connection.js
@@ -15,7 +15,7 @@ const pool = new Pool({
     password: process.env.DB_PASSWORD,
 
     // Pool configuration - DO NOT MODIFY without load testing
-    max: 20,              // Maximum connections (see DECISION-DB-001)
+    max: 50,              // Pool size limited to 20 as per DECISION-DB-001
     min: 5,               // Minimum connections
     idleTimeoutMillis: 30000,
     connectionTimeoutMillis: 2000,
--- src/legacy/old-auth.js
+++ src/legacy/old-auth.js
@@ -10,5 +10,7 @@
 // Legacy code for testing deprecated decision status
 module.exports = {
     deprecated: true,
-    message: 'This file is only for testing deprecated decisions'
+    message: 'This file is only for testing deprecated decisions',
+    testStatus: 'deprecated check',
+    newField: 'testing-change' // Added to verify if this triggers an alert (should be ignored)
 };
